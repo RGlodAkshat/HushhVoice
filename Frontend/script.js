9052d794ba449c6b1cd7a5fc4503b8102ac6afb9
Akshat <ak8akshat@gmail.com>
2025-09-22 14:13:27 +0530

    added sign in bugs fixs

--- Frontend/script.js
+++ Frontend/script.js
@@ -71,7 +71,7 @@ const els = {
    1) CONFIG & STORAGE KEYS
    ============================= */
 const CONFIG = {
-  BASE_URL: "https://503f19bf9c1a.ngrok-free.app",
+  BASE_URL: "https://hushhvoice-1.onrender.com",
   CLIENT_ID: "106283179463-48aftf364n2th97mone9s8mocicujt6c.apps.googleusercontent.com",
 
   GMAIL_SCOPES: [
