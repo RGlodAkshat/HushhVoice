8fa8a6b41e94fc6317c07e72003b498f19acffda
Akshat <ak8akshat@gmail.com>
2025-09-22 09:30:09 +0530

    Point frontend to Vercel API prefix /api

--- Frontend/script.js
+++ Frontend/script.js
@@ -71,7 +71,7 @@ const els = {
    1) CONFIG & STORAGE KEYS
    ============================= */
 const CONFIG = {
-  BASE_URL: "",
+  BASE_URL: "/api",
   CLIENT_ID: "106283179463-48aftf364n2th97mone9s8mocicujt6c.apps.googleusercontent.com",
 
   GMAIL_SCOPES: [
